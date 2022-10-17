e21c176822690f95faf4d73837f81538a2f5f2ca
ryomayukumoto <ryomayukkun@gmail.com>
2022-10-17 09:23:33 +0900

    prefab修正中

--- Dragon/Assets/Script/Item/Slime/SlimeController.cs
+++ Dragon/Assets/Script/Item/Slime/SlimeController.cs
@@ -6,50 +6,52 @@ public class SlimeController : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] prefabSlime;   // スライム配列
+  
     [SerializeField]
-    private float sponTime = 10f;
+    private float sponTime = 10f;  // スポーンするまでのインターバル
     [SerializeField]
     private float pos_z = 0;
     private int slime_number;
 
-    public int Slime_counter;
+    public int Slime_counter;   // Scene内のスライムの数
     [SerializeField]
-    private int slime_Max = 10;
-    private float pos_x = 50f;
-    private float pos_y = 50f;
-    private Vector3 spon_pos;
+    private int slime_Max = 10;   // Scene内のスライムの最大数
+    private float pos_x = 50f;    // スポーン範囲制限用(x座標)
+    private float pos_y = 50f;    // スポーン範囲制限用(y座標)
+    
 
     private bool isInsideCamera;    // カメラの範囲内にいるか
 
     void Start()
     {
-        Slime_counter = 0;
+        Slime_counter = 0;      // Scene内のスライムの数初期化
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Slime_counter < slime_Max)
-            spon_pos = random();
-        // 画面内にいるときspon
-        if (isInsideCamera)
-            StartCoroutine("spon");
+        if (Slime_counter < slime_Max)  // スライムの数が最大値じゃないとき
+            random();        // スポーン座標をランダムに設定
+           
+        
+        if (isInsideCamera)             // カメラに映っているとき
+             StartCoroutine("spon");    // 設定した座標にスポーンさせる
     }
 
-    private Vector3 random()
+    private Vector3 random()  // スポーン座標設定関数(ランダムに設定)
     {
         slime_number = Random.Range(0, prefabSlime.Length);
         float x = Random.Range(-pos_x, pos_x);
         float y = Random.Range(-pos_y, pos_y);
 
-        Vector3 pos = new Vector3(x, y, pos_z);
+        Vector3 pos = new Vector3(x, y, pos_z);  // スライムのスポーン座標
         return pos;
     }
     private IEnumerator spon(Vector3 pos)
     {
         yield return new WaitForSeconds(sponTime);  
-        Instantiate(prefabSlime[slime_number], pos, Quaternion.identity);
-        Slime_counter++;
+        Instantiate(prefabSlime[slime_number], pos, Quaternion.identity);  // インスタンス化
+        Slime_counter++;  // Scene内のスライムの数(+1)
         //Instantiate(prefabItem[item_number], pos, Quaternion.identity);
     }
 
