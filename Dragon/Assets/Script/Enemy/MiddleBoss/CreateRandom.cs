c5ae747a5111ca602848f7559c9741af73cbb876
ryomayukumoto <ryomayukkun@gmail.com>
2022-10-18 16:55:02 +0900

    prefab生成

--- Dragon/Assets/Script/Enemy/MiddleBoss/CreateRandom.cs
+++ Dragon/Assets/Script/Enemy/MiddleBoss/CreateRandom.cs
@@ -25,24 +25,33 @@ public class CreateRandom : MonoBehaviour
    
     void Update()
     {
-        // 
+        // 生成最大数 > 画面内のPrefab数の時
         if(count > 0)
         {
+            // Timerコルーチン呼び出し
             StartCoroutine("Timer");
         }
     }
 
+    /**
+    * @brief Prefab配列からランダムで、エリアのランダム位置に生成する関数
+    * @note  中ボスの生成位置についてはまた考える
+    */
     private void random()
     {
-        number = Random.Range(0,prefabEnemy.Length);
+        // 生成するPrefabのIndexを配列の要素の中からランダムに設定
+        number = Random.Range(0,prefabEnemy.Length); 
 
-        float x = Random.Range(-posX,posX);
-        float y = Random.Range(-posY,posY);
+        float x = Random.Range(-posX,posX);  // フィールドの範囲内のx座標をランダムで設定
+        float y = Random.Range(-posY,posY);  // フィールドの範囲内のy座標をランダムで設定
 
-        Vector3 pos = new Vector3(x,y,posZ);
-        Instantiate(prefabEnemy[number], pos, Quaternion.identity);
-        count--;
+        Vector3 pos = new Vector3(x,y,posZ); // 設定した座標をもとに生成posを設定
+        Instantiate(prefabEnemy[number], pos, Quaternion.identity);// 設定したposにPrefab生成
+        count--;  // 生成数++
     }
+
+    // Timerコルーチン
+    // 生成待機時間後 => random()呼び出し => random()の中で生成
     private IEnumerator Timer()
     {
         yield return new WaitForSeconds(timer);
