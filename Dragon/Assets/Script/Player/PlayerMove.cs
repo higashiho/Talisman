19741be338386df580c39c972b651b8b21847459
higashihokakeru <kakeru.0422.pojr@gmail.com>
2022-10-18 18:38:19 +0900

    微修正

--- Dragon/Assets/Script/Player/PlayerMove.cs
+++ Dragon/Assets/Script/Player/PlayerMove.cs
@@ -7,6 +7,9 @@ public class PlayerMove : MonoBehaviour
     public Vector2 PlayerSpeed = new Vector2(5.0f, 5.0f);    // Playerの移動速度
     private Vector2 pos;    // playerの位置を保存する変数
 
+
+    public int Hp;                  //ヒットポイント
+    private bool onShield = true;       //シールドがあるか
     // Start is called before the first frame update
     void Start()
     {
@@ -38,7 +41,11 @@ public class PlayerMove : MonoBehaviour
         {
             pos.x += PlayerSpeed.x * Time.deltaTime;    // 右移動
         }
-
         transform.position = pos;
+    }
+
+    private void shield()
+    {
+
     }
 }
