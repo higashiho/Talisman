57286eb5256508d3dca81884969d3e6652ebb8a9
SakuraHatta <elmo.sakura27@gmail.com>
2022-10-19 18:48:59 +0900

    スクリプト作成

---
+++ Dragon/Assets/Script/UI/HPManger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPManger : MonoBehaviour
+{
+    private int lifeCount;      // HPをカウントする
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
