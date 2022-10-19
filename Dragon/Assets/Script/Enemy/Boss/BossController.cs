54cb6d728909b89472c8a9cdabded383d2df7175
higashihokakeru <kakeru.0422.pojr@gmail.com>
2022-10-19 18:14:30 +0900

    変更追加

--- Dragon/Assets/Script/Enemy/Boss/BossController.cs
+++ Dragon/Assets/Script/Enemy/Boss/BossController.cs
@@ -6,22 +6,23 @@ public class BossController : MonoBehaviour
 {
 
     [HeaderAttribute("目標座標"), SerializeField]
-    private List<Vector3> destinations;     //目標座標
+    private Vector3 targetCoordinates;
+    // private List<Vector3> destinations;     //目標座標
 
     [HeaderAttribute("NavMeshAgent2D"), SerializeField]
    private NavMeshAgent2D agent; //NavMeshAgent2Dを使用するための変数
 
-    [SerializeField, ]
-   private int randomNumber = 0;        // list　index指定用
+    // [SerializeField, ]
+    // private int randomNumber = 0;        // list　index指定用
 
    
-   private int minNumber = 1;           // rondomNumber最小値
+   // private int minNumber = 1;           // rondomNumber最小値
    private int numericPreservation;      // 前回randoNumber保存用
 
-    [SerializeField, HeaderAttribute("初期移動時間")]
-   private int startTime = 60;    // 初期ランダムナンバー設定時間
-   [SerializeField, HeaderAttribute("定期移動時間")]
-   private int waitTime = 30;     // ２回目以降待ち時間
+    // [SerializeField, HeaderAttribute("初期移動時間")]
+    // private int startTime = 60;    // 初期ランダムナンバー設定時間
+    // [SerializeField, HeaderAttribute("定期移動時間")]
+    // private int waitTime = 30;     // ２回目以降待ち時間
 
    [SerializeField]
    private Transform player;       // player格納用
@@ -31,7 +32,7 @@ public class BossController : MonoBehaviour
 
    private int attackTime = 15;   // アタック間隔
 
-   private float posX = 44.0f, posY = 44.0f;        // 座標
+   // private float posX = 44.0f, posY = 44.0f;        // 座標
 
    [HeaderAttribute("ヒットポイント")]
    public int Hp;
@@ -39,44 +40,50 @@ public class BossController : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        destinations = new List<Vector3>();
-        destinations.Add(new Vector3(0,0,0));               // 初期値
-        destinations.Add(new Vector3(posX, posY, 0));     // 右上
-        destinations.Add(new Vector3(posX, -posY, 0));     // 右下
-        destinations.Add(new Vector3(-posX, posY, 0));     // 左上
-        destinations.Add(new Vector3(-posX, -posY, 0));   // 左下
+        // destinations = new List<Vector3>();
+        // destinations.Add(new Vector3(0,0,0));               // 初期値
+        // destinations.Add(new Vector3(posX, posY, 0));     // 右上
+        // destinations.Add(new Vector3(posX, -posY, 0));     // 右下
+        // destinations.Add(new Vector3(-posX, posY, 0));     // 左上
+        // destinations.Add(new Vector3(-posX, -posY, 0));   // 左下
     }
     void Start()
     {
         agent = GetComponent<NavMeshAgent2D>(); //agentにNavMeshAgent2Dを取得
         
-        InvokeRepeating("randIndex", startTime, waitTime);
+        // InvokeRepeating("randIndex", startTime, waitTime);
         InvokeRepeating("attack", attackTime, attackTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(randomNumber != 0)
-            move();
+        // if(randomNumber != 0)
+            // move();
+
+
     }
 
-    //指定秒ごとにdestinationsのindexを変更
-    private void randIndex()
-    {
-        numericPreservation = randomNumber;
-        randomNumber = Random.Range(minNumber, destinations.Count);
-        while(numericPreservation == randomNumber)
-        {
-            randomNumber = Random.Range(minNumber, destinations.Count);
-        }
-    }
+    // 指定秒ごとにdestinationsのindexを変更
+    // private void randIndex()
+    // {
+    //     numericPreservation = randomNumber;
+    //     randomNumber = Random.Range(minNumber, destinations.Count);
+    //     while(numericPreservation == randomNumber)
+    //     {
+    //         randomNumber = Random.Range(minNumber, destinations.Count);
+    //     }
+    // }
 
+    // private void move()
+    // {
+    //     agent.SetDestination(destinations[randomNumber]);
+    // }
+    
     private void move()
     {
-        agent.SetDestination(destinations[randomNumber]);
+        agent.SetDestination(targetCoordinates);
     }
-    
     private void attack()
     {
         Instantiate(meteorite, player.position, Quaternion.identity);
@@ -85,6 +92,6 @@ public class BossController : MonoBehaviour
     
     void OnDestroy()
     {
-        destinations.Clear();
+        //destinations.Clear();
     }
 }
