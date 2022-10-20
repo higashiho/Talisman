688600ab02e91e699f027e6c0bd01b50ecd1b02c
SakuraHatta <elmo.sakura27@gmail.com>
2022-10-20 18:01:41 +0900

    ライフ表示の画像追加 スクリプト追記

--- Dragon/Assets/Script/UI/HPManger.cs
+++ Dragon/Assets/Script/UI/HPManger.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class HPManger : MonoBehaviour
 {
-    private int lifeCount;      // HPをカウントする
+    private int lifeCount;      // HPをカウントする     // あとでpublicに変更する
+    private int maxLife = 3;
+    [SerializeField]
+    private GameObject[] lifeArray = new GameObject[3];
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i=0; i<maxLife; i++){
+            lifeArray[i].gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*for(int i=0; i<lifeCount; i++){
+            lifeArray[i].gameObject.SetActive(true);
+        }*/     // ここまだ途中
     }
 }
