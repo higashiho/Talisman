a95611620f2d6afcf803dc609a84c6402a05185d
humihumiinoue <shyallandboill1212@gmail.com>
2022-10-23 22:40:31 +0900

    エネミーの移動修正

--- Dragon/Assets/Script/Enemy/NomalEnemy/Chase_Enemy.cs
+++ Dragon/Assets/Script/Enemy/NomalEnemy/Chase_Enemy.cs
@@ -7,19 +7,14 @@ public class Chase_Enemy : MonoBehaviour
 
     [SerializeField]
     private GameObject player;        //プレイヤーを取得
-    private Vector3 PlayerPosition;   //プレイヤーの位置
-    private Vector3 EnemyPosition;    //エネミーの位置
 
-
-
+    //プレイヤーを追いかけるスピードの減速補正
+    private float enemyChaseSpeed = 0.2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
 
-        PlayerPosition = player.transform.position;
-        EnemyPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -27,22 +22,49 @@ public class Chase_Enemy : MonoBehaviour
     {
 
     }
+
     //一定時間ごとに処理
-    //遠いほど早く近いほど遅く追いかける
     private void FixedUpdate()
     {
-        PlayerPosition = player.transform.position;
-        EnemyPosition = transform.position;
 
-        EnemyPosition.x += (PlayerPosition.x - EnemyPosition.x) * 0.012f;
-        EnemyPosition.y += (PlayerPosition.y - EnemyPosition.y) * 0.012f;
-        transform.position = EnemyPosition;
+        Vector3 playerv = player.transform.position;//プレイヤーの位置
+        Vector3 enemyv = transform.position;        //敵の位置
+ 
+        float p_vX = playerv.x - enemyv.x;          //プレイヤーと敵のx座標位置の差
+        float p_vY = playerv.y - enemyv.y;          //プレイヤーと敵のy座標位置の差
+ 
+        float vx = 0f;
+        float vy = 0f;
+ 
+        float movespeed = 10f;//移動読度
+ 
+        // 減算した結果がマイナスであればXは減算処理
+        if ( p_vX < 0 ) 
+        {
+            vx -= movespeed * enemyChaseSpeed;
+        } 
+        else 
+        {
+            vx = movespeed * enemyChaseSpeed;
+        }
+ 
+        // 減算した結果がマイナスであればYは減算処理
+        if ( p_vY < 0 ) 
+        {
+            vy -= movespeed * enemyChaseSpeed;
+        } 
+        else
+         {
+            vy = movespeed * enemyChaseSpeed;
+        }
+ 
+        transform.Translate(vx/50, vy/50, 0);
+ 
     }
-
     //プレイヤーに当たったら消える
     private void OnTriggerEnter2D(Collider2D other)
     {
-    if(other.gameObject.tag == "Player")
+    if(other.gameObject.tag == "Bullet")
         {
             Destroy(this.gameObject);
         }
