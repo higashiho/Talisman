464f9ad97c9abd00a00c6706848612c754dd5cab
humihumiinoue <shyallandboill1212@gmail.com>
2022-10-24 17:33:47 +0900

    swordにリジッドボディ追加

--- Dragon/Assets/Script/Enemy/NomalEnemy/ChaseEnemy.cs
+++ Dragon/Assets/Script/Enemy/NomalEnemy/ChaseEnemy.cs
@@ -15,17 +15,26 @@ public class ChaseEnemy : MonoBehaviour
 
     [SerializeField]
     private float destroytimer;     //敵が自動消滅する時間
+
+    [SerializeField]
+    private Renderer _dieEnemy;     //点滅対象
+    [SerializeField]
+    private float _cycle = 1;       //点滅周期（秒）
+    private float _throwTime;       //経過時間を図るやつ
+    private float correction = 0.5f;//点滅間隔の補正
+
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
-        Destroy(this.gameObject,destroytimer);
+        Destroy(this.gameObject, destroytimer);
     }
 
-    // Update is called once per frame
+    // Upda結果、 is called once per frame
     void Update()
     {
-       
+        
     }
     //一定時間ごとに処理
     //遠いほど早く近いほど遅く追いかける
@@ -39,16 +48,27 @@ public class ChaseEnemy : MonoBehaviour
         transform.position = EnemyPosition;
     }
 
-    //プレイヤーに当たったら消える
-    private void OnTriggerEnter2D(Collider2D other)
+    //プレイヤーの剣攻撃に当たったら消える
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        if(other.gameObject.name == "Sword")
+        if(col.gameObject.tag == "Sword")
         {
             Instantiate(ItemPrefab,this.transform.position,Quaternion.identity);
             Destroy(this.gameObject);
+            //flash();
         }
     }
 
-    
-    
+    private void flash()
+    {
+        //経過時間
+       _throwTime += Time.deltaTime;
+
+       //_cycle周期で繰り返す値の取得
+       //結果、0～cycleの範囲が得られる
+       var repeatValue = Mathf.Repeat(_throwTime,_cycle);
+
+        //内部時刻timeにおける明滅状態を反映
+        _dieEnemy.enabled = repeatValue >= _cycle * correction;
+    }
 }
