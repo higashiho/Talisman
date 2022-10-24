05af2575455cf5b5eb66e4cbe4d4c059e34e7ebb
higashihokakeru <kakeru.0422.pojr@gmail.com>
2022-10-24 14:26:05 +0900

    ビーム生成

--- Dragon/Assets/Script/Enemy/Boss/BeamController.cs
+++ Dragon/Assets/Script/Enemy/Boss/BeamController.cs
@@ -6,35 +6,51 @@ public class BeamController : MonoBehaviour
 {
 
     private GameObject Player;      // プレイヤー格納用
+    
 
     private Vector3 posPlayer;      // プレイヤーの座標格納用
 
     [SerializeField]
     private Vector3 scaleUpSpeed;    // scaleの拡大スピード
 
-    private Vector3 relativePos;        // プレイヤーと自身の角度
+    public int Damege = 3;           // playerに与えるダメージ
 
-    private Quaternion rotation;    // 角度代入用
+    [SerializeField, HeaderAttribute("消えるまでの時間")]
+    private float destroyTime;      // 消えるまでの時間
+    [SerializeField]
+    private GameObject beamLine;        // ビームラインオブジェクト
+
+    private float waitTime = 2.0f;      // ビームが伸びるまでの時間
+    private bool wait = false;          // ビームが伸びれるか
+
     // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.FindWithTag("Player");
-        posPlayer = Player.transform.position;
-        relativePos = posPlayer - this.transform.position;
-        rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        transform.LookAt(Player.transform.position);
+        Destroy(this.gameObject,destroyTime);
+
+       StartCoroutine(offWate());
     }
 
     // Update is called once per frame
     void Update()
     {
-        beamBehaviour();
+        if(wait)
+            beamBehaviour();
     }
 
     // ビームの挙動
     private void beamBehaviour()
     {
-        this.transform.rotation = rotation;
-
         this.transform.localScale += scaleUpSpeed * Time.deltaTime;
+    }
+
+    private IEnumerator offWate()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        Destroy(beamLine.gameObject);
+        wait = true;
     }
 }
