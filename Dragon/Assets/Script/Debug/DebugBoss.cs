5f94821b9720cce6938585e11a36929be0e76184
higashihokakeru <kakeru.0422.pojr@gmail.com>
2022-10-24 16:52:58 +0900

    デバック機能追加

--- Dragon/Assets/Script/Debug/DebugBoss.cs
+++ Dragon/Assets/Script/Debug/DebugBoss.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class DebugBoss : MonoBehaviour
 {
+    [SerializeField]
     private BossController bossController;          // スクリプト格納用
+
+    [SerializeField]
+    private GameObject player;                      // プレイヤー
+    [SerializeField]
+    private GameObject boss;                        // boss
+    private Vector3 pos;                            // 座標
+    private float playerPosY = 11.0f;               // 転移時プレイヤーのｙ座標
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +22,44 @@ public class DebugBoss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bossMoveArea();
     }
 
-    private void moveArea()
+    // エリア転移
+    private void bossMoveArea()
     {
+        // posYの初期化
+        pos.y = 0;
 
+        // 指定キーを押すとそこに移動
+        if(Input.GetKeyDown("1"))
+        {
+            pos.x = bossController.Areas[0];
+            boss.transform.position = pos;
+            pos.y = playerPosY;
+            player.transform.position = pos;
+        }
+        if(Input.GetKeyDown("2"))
+        {
+            pos.x = bossController.Areas[1];
+            boss.transform.position = pos;
+            pos.y = playerPosY;
+            player.transform.position = pos;
+        }
+        if(Input.GetKeyDown("3"))
+        {  
+            pos.x = bossController.Areas[2];
+            boss.transform.position = pos;
+            pos.y = playerPosY;
+            player.transform.position = pos;
+        }
+        if(Input.GetKeyDown("4"))
+        {
+            pos.x = bossController.Areas[3];
+            boss.transform.position = pos;
+            pos.y = playerPosY;
+            player.transform.position = pos;
+        }
     }
+
 }
