6aa7dac62723d53c63c2b65b2deea1dd6a0bd55f
sakura hatta <elmo.sakura27@gmail.com>
2022-10-31 14:41:23 +0900

    hp振動

---
+++ Dragon/Assets/Script/UI/HPShake.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPShake : MonoBehaviour
+{
+    [SerializeField]
+    private Transform hpPos;
+    [SerializeField]
+    private float shakePower = 0;     // 揺れの強さ
+    private Vector3 hpInitPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpInitPos =hpPos.position;     // 開始時の位置を保存
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hpPos.position = hpInitPos + Random.insideUnitSphere * shakePower;      // ランダムに揺らす
+    }
+}
