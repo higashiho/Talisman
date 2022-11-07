376efb7cf722720187535cea121f4d6a43c0f4ba
higashihokakeru <kakeru.0422.pojr@gmail.com>
2022-11-07 15:24:37 +0900

    削除を解除

---
+++ Dragon/Assets/Script/FindBoss.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindBoss : MonoBehaviour
+{
+
+    private GameObject boss;
+    private BossController bossController;
+
+    private bool onFind = false;
+
+    public GameObject GetBoss() {return boss;}
+    public BossController GetBossController() {return bossController;}
+    public bool GetOnFind() {return onFind;}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BossFind()
+    {
+        boss = GameObject.FindWithTag("Boss");
+        bossController = boss.GetComponent<BossController>();
+        onFind = true;
+    }
+}
